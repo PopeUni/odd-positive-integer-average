a939678ab26392d7e8f25aabd882a6b8338a7043
Jacob <jakemetty@hotmail.com>
2020-04-23 05:03:07 +1000

    server configured with cli and gui

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,31 @@
-import express from 'express';
-import { averageOddPositiveIntegers } from './util';
+import express from "express";
+import { averageOddPositiveIntegers } from "./util";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 /**
  * This file should default export the express application - this is for `index.spec.ts` to be able to test it.
  */
 
-console.log("server is starting");
-
-const data: number[] = [1,2,3,4,5];
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json(), cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/odd-positive-average', (req, res) => {
+app.post("/odd-positive-average", (req, res) => {
+  console.log(req.body);
 
-    const result: number = averageOddPositiveIntegers(data);
+  if (Object.keys(req.body).length === 0 && req.body.constructor === Object) {
+    res.status(400).send({ message: "post request was empty" });
+    console.log("post request was empty");
+  } else {
+    res.set({ "content-type": "application/json" });
+    res.status(200).send({ data: req.body });
+  }
+});
 
-        if(result == 0)
-        {
-            res.status(400).send("no odd positive numbers in the array");
-
-        }
-        else
-        {   res.set({ 'content-type': 'application/json'});
-            res.status(200).send({result});
-        }
-
-    
- });
-
-app.listen(4442,listening);
-
-
-function listening(){
-
-    console.log("listening...");
-    //console.log(result);
-}
+app.listen(3003, () => {
+  console.log("server has started");
+});
 
 export default app;
-
--- src/util.ts
+++ src/util.ts
@@ -3,15 +3,14 @@
  */
 export function averageOddPositiveIntegers(input: number[]): number {
 
-  var Average = 1;
-
   const oddPosNumberArray: number[] = input.filter(positivenumber =>(positivenumber > 0 && positivenumber % 2 != 0 ));
   
-  if(oddPosNumberArray.length == 0){
-        Average = 0; //if length is zero than no odd positive numbers in the array
+  if(oddPosNumberArray.length === 0){
+        return 0; //if length is zero than no odd positive numbers in the array
   }else {
         const total: number = oddPosNumberArray.reduce((accumulator,currentValue) => accumulator + currentValue);
-        Average = total/oddPosNumberArray.length;
+        return total/oddPosNumberArray.length; //calculates total
+
  }
-  return Average;
+  
 }
